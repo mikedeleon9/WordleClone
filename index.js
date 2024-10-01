5993411b0c6957a3862ec7483f5dc57d03d62fd0
mikedeleon9 <mikedeleon9@gmail.com>
2024-10-01 19:37:12 -0400

    starting projecT

---
+++ index.js
@@ -0,0 +1,114 @@
+
+const mainContainer = document.querySelector(".main-container");
+let currentBoxIndex = 0;
+const hiddenWord = ["P","A","R","T","Y"];
+const totalBoxes = 30;
+const letters = 'abcdefghijklmnopqrstuvwxyz';
+const splitLetters = letters.split(',');
+let rowIndex = 0;
+  
+
+for(x = 0; x < totalBoxes; x++){
+  const boxes = document.createElement('div');
+  boxes.className = "box";
+   
+
+  mainContainer.appendChild(boxes)
+}
+
+const boxes = document.querySelectorAll('.box');
+  
+
+
+
+const boxesArray = [];
+
+boxes.forEach(box => {
+  boxesArray.push(box);
+  box.classList.add('box')
+})
+
+const rows = [];
+const itemsPerRow = 5;
+
+
+for(let i = 0; i < boxesArray.length; i += itemsPerRow){
+  rows.push(boxesArray.slice(i, i + itemsPerRow))
+}
+
+let currentRow = rows[rowIndex];
+
+
+
+
+
+
+ document.addEventListener('keydown',function(event) {
+       const keyPressed = event.key;
+       const currentBox = currentRow[currentBoxIndex];
+   
+   
+   
+       if(keyPressed !== "Backspace" && letters.includes(keyPressed) && currentBoxIndex < currentRow.length){
+
+                currentBox.textContent = keyPressed.toUpperCase();
+                
+                currentBox.classList.add('black-border');
+                currentBoxIndex++;
+         
+            
+            }
+   
+ 
+   
+         if(keyPressed === "Backspace" && currentBoxIndex > 0){   
+           
+           
+            currentBoxIndex--;
+           const previousBox = boxes[currentBoxIndex];
+           previousBox.textContent = "";
+           previousBox.classList.remove('black-border');
+           
+         }
+   
+
+   
+        if(keyPressed === "Enter"){
+          checkGuess();
+          
+        }
+  
+   })
+
+function checkGuess(){
+  const guess = Array.from(currentRow).map(box=> box.textContent.trim().toLowerCase());
+  
+for(let i = 0; i < guess.length; i++){
+  if(guess[i] === hiddenWord[i].toLowerCase()){
+    currentRow[i].classList.add('correct');
+    
+  }
+  else{
+    currentRow[i].classList.add('incorrect');
+  }
+}
+  rowIndex++;
+  currentRow = rows[rowIndex];
+  currentBoxIndex = 0;
+}
+   
+  
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
